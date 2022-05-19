95e218b628b665598b051e94e429e8ce1ce3cdd4
tj-tieso <tj.tieso@gmail.com>
2022-05-19 13:18:11 -0500

    green: isAlive() and reduceHealth()

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -34,3 +34,32 @@ test("gets inventory from player or returns false", () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player health value", () => {
+  const player = new Player("Gorble");
+
+  expect(player.getHealth()).toEqual(
+    expect.stringContaining(player.health.toString())
+  );
+});
+
+test("checks to see if player is Alive", () => {
+  const player = new Player("Gorble");
+
+  expect(player.isAlive()).toBe(true);
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBe(false);
+});
+
+test("subtracts from player health", () => {
+  const player = new Player("Gorble");
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+  expect(player.health).toBe(0);
+});
